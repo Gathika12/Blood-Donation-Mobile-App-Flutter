30c323ba6689b9d6ed358aa54943ccfd220dd929
Nuwan Darshana <darshananuwan958@gmail.com>
2024-05-29 01:18:17 +0530

    change icons in donate pade

--- blood donation/lib/donate.dart
+++ blood donation/lib/donate.dart
@@ -26,6 +26,9 @@ class _RegisterPageState extends State<DonatePage> {
     required TextEditingController controller,
     required String labelText,
     required IconData icon,
+    required BuildContext context,
+    bool isDateField = false,
+    bool isTimeField = false,
   }) {
     return Container(
       margin: EdgeInsets.symmetric(vertical: 8.0),
@@ -41,36 +44,59 @@ class _RegisterPageState extends State<DonatePage> {
           ),
         ],
       ),
-      child: TextFormField(
-        controller: controller,
-        decoration: InputDecoration(
-          labelText: labelText,
-          prefixIcon: Icon(
-            icon,
-            color: Colors.blueAccent,
+      child: Row(
+        children: [
+          Expanded(
+            child: TextFormField(
+              controller: controller,
+              decoration: InputDecoration(
+                labelText: labelText,
+                prefixIcon: Icon(
+                  icon,
+                  color: Colors.blueAccent,
+                ),
+                border: InputBorder.none,
+                contentPadding: EdgeInsets.only(
+                    left: 20.0, right: 10.0, top: 16.0, bottom: 16.0),
+              ),
+            ),
           ),
-          border: InputBorder.none,
-          contentPadding:
-              EdgeInsets.symmetric(horizontal: 20.0, vertical: 16.0),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildGenderDropdown() {
-    return Container(
-      margin: EdgeInsets.symmetric(vertical: 8.0),
-      padding: EdgeInsets.symmetric(horizontal: 20.0),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(30.0),
-        border: Border.all(color: Colors.red),
-        color: Colors.white,
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black12,
-            blurRadius: 5.0,
-            offset: Offset(0, 2),
-          ),
+          if (isDateField)
+            IconButton(
+              icon: Icon(Icons.calendar_today),
+              onPressed: () async {
+                final DateTime? pickedDate = await showDatePicker(
+                  context: context,
+                  initialDate: DateTime.now(),
+                  firstDate: DateTime(2000),
+                  lastDate: DateTime(2101),
+                );
+                if (pickedDate != null) {
+                  final String formattedDate =
+                      "${pickedDate.year}-${pickedDate.month}-${pickedDate.day}";
+                  setState(() {
+                    controller.text = formattedDate;
+                  });
+                }
+              },
+            ),
+          if (isTimeField)
+            IconButton(
+              icon: Icon(Icons.access_time),
+              onPressed: () async {
+                final TimeOfDay? pickedTime = await showTimePicker(
+                  context: context,
+                  initialTime: TimeOfDay.now(),
+                );
+                if (pickedTime != null) {
+                  final String formattedTime =
+                      "${pickedTime.hour}:${pickedTime.minute}";
+                  setState(() {
+                    controller.text = formattedTime;
+                  });
+                }
+              },
+            ),
         ],
       ),
     );
@@ -104,31 +130,39 @@ class _RegisterPageState extends State<DonatePage> {
                 controller: _fullNameController,
                 labelText: 'Full Name',
                 icon: Icons.person,
+                context: context,
               ),
               _buildOvalTextFieldWithIcon(
                 controller: _LocationController,
                 labelText: 'Location',
                 icon: Icons.location_city,
+                context: context,
               ),
               _buildOvalTextFieldWithIcon(
                 controller: _DateController,
                 labelText: 'Date',
                 icon: Icons.date_range,
+                context: context,
+                isDateField: true,
               ),
               _buildOvalTextFieldWithIcon(
                 controller: _TimeController,
                 labelText: 'Time',
                 icon: Icons.time_to_leave,
+                context: context,
+                isTimeField: true,
               ),
               _buildOvalTextFieldWithIcon(
                 controller: _PriordonationController,
                 labelText: 'prior donation',
                 icon: Icons.card_giftcard,
+                context: context,
               ),
               _buildOvalTextFieldWithIcon(
                 controller: _AllergiesController,
                 labelText: 'Allergies/diseases',
                 icon: Icons.lock,
+                context: context,
               ),
               SizedBox(height: 16.0),
               ElevatedButton(
